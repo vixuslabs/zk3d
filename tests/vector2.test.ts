5cb6fa16761ec8e0fa80d96a5cfc3ef1db318227
Rafael Campos <racampos@gmail.com>
2024-03-01 21:44:48 -0600

    Refactors the tests for Vector2 and Vector3 to use a more efficient way of comparing values.

--- tests/vector2.test.ts
+++ tests/vector2.test.ts
@@ -1,8 +1,6 @@
 import * as ZK3D from '../src/index';
 import * as THREE from 'three';
 
-const round = (x: number, precision: number) => Math.round(x * Math.pow(10, precision)) / Math.pow(10, precision);
-
 describe('Vector2 operations', () => {
   const v1 = [0.01234, 2.345];
   const v2 = [4.567, 0.05678];
@@ -14,109 +12,88 @@ describe('Vector2 operations', () => {
 
   test('vector addition', () => {
     const threejsResult = v1t.clone().add(v2t);
-    const threejsResultArray = [threejsResult.x, threejsResult.y].map(x => round(x, 4));
     const zk3dResult = v1z.clone().add(v2z);
-    const zk3dResultArray = [zk3dResult.x.toNumber(), zk3dResult.y.toNumber()].map(x => round(x, 4));
-    expect(threejsResultArray).toEqual(zk3dResultArray);
+    expect(zk3dResult.x.toNumber()).toBeCloseTo(threejsResult.x, 4);
+    expect(zk3dResult.y.toNumber()).toBeCloseTo(threejsResult.y, 4);
   });
 
   test('vector subtraction', () => {
     const threejsResult = v1t.clone().sub(v2t);
-    const threejsResultArray = [threejsResult.x, threejsResult.y].map(x => round(x, 4));
     const zk3dResult = v1z.clone().sub(v2z);
-    const zk3dResultArray = [zk3dResult.x.toNumber(), zk3dResult.y.toNumber()].map(x => round(x, 4));
-    expect(threejsResultArray).toEqual(zk3dResultArray);
+    expect(zk3dResult.x.toNumber()).toBeCloseTo(threejsResult.x, 4);
+    expect(zk3dResult.y.toNumber()).toBeCloseTo(threejsResult.y, 4);
   });
 
   test('vector multiplication', () => {
     const threejsResult = v1t.clone().multiply(v2t);
-    const threejsResultArray = [threejsResult.x, threejsResult.y].map(x => round(x, 3));
     const zk3dResult = v1z.clone().multiply(v2z);
-    const zk3dResultArray = [zk3dResult.x.toNumber(), zk3dResult.y.toNumber()].map(x => round(x, 3));
-    expect(threejsResultArray).toEqual(zk3dResultArray);
+    expect(zk3dResult.x.toNumber()).toBeCloseTo(threejsResult.x, 3);
+    expect(zk3dResult.y.toNumber()).toBeCloseTo(threejsResult.y, 3);
   });
 
   test('multiplication by scalar', () => {
     const scalar = 3;
     const threejsResult = v1t.clone().multiplyScalar(scalar);
-    const threejsResultArray = [threejsResult.x, threejsResult.y].map(x => round(x, 3));
     const zk3dResult = v1z.clone().multiplyScalar(ZK3D.Real64.from(scalar));
-    const zk3dResultArray = [zk3dResult.x.toNumber(), zk3dResult.y.toNumber()].map(x => round(x, 3));
-    expect(threejsResultArray).toEqual(zk3dResultArray);
+    expect(zk3dResult.x.toNumber()).toBeCloseTo(threejsResult.x, 3);
+    expect(zk3dResult.y.toNumber()).toBeCloseTo(threejsResult.y, 3);
   });
 
   test('dot product', () => {
     const threejsResult = v1t.dot(v2t);
-    const zk3dResult = v1z.clone().dot(v2z);
-    expect(round(threejsResult, 2)).toEqual(round(zk3dResult.toNumber(), 2));
+    const zk3dResult = v1z.dot(v2z).toNumber();
+    expect(zk3dResult).toBeCloseTo(threejsResult, 2);
   });
-
-  // test('cross product', () => {
-  //   const threejsResult = v1t.clone().cross(v2t);
-  //   const threejsResultArray = [threejsResult.x, threejsResult.y].map(x => round(x, 3));
-  //   const zk3dResult = v1z.clone().cross(v2z);
-  //   const zk3dResultArray = [zk3dResult.x.toNumber(), zk3dResult.y.toNumber()].map(x => round(x, 3));
-  //   expect(threejsResultArray).toEqual(zk3dResultArray);
-  // });
 
   test('length squared', () => {
     const threejsResult = v1t.lengthSq();
-    const zk3dResult = v1z.clone().lengthSq();
-    expect(round(threejsResult, 3)).toEqual(round(zk3dResult.toNumber(), 3));
+    const zk3dResult = v1z.lengthSq().toNumber();
+    expect(zk3dResult).toBeCloseTo(threejsResult, 3);
   });
 
   test('distanceToSquared', () => {
     const threejsResult = v1t.distanceToSquared(v2t);
-    const zk3dResult = v1z.clone().distanceToSquared(v2z);
-    expect(round(threejsResult, 3)).toEqual(round(zk3dResult.toNumber(), 3));
+    const zk3dResult = v1z.distanceToSquared(v2z).toNumber();
+    expect(zk3dResult).toBeCloseTo(threejsResult, 3);
   });
 
   test('lerp', () => {
     const alpha = 0.5;
     const threejsResult = v1t.clone().lerp(v2t, alpha);
-    const threejsResultArray = [threejsResult.x, threejsResult.y].map(x => round(x, 3));
     const zk3dResult = v1z.clone().lerp(v2z, ZK3D.Real64.from(alpha));
-    const zk3dResultArray = [zk3dResult.x.toNumber(), zk3dResult.y.toNumber()].map(x => round(x, 3));
-    expect(threejsResultArray).toEqual(zk3dResultArray);
+    expect(zk3dResult.x.toNumber()).toBeCloseTo(threejsResult.x, 3);
+    expect(zk3dResult.y.toNumber()).toBeCloseTo(threejsResult.y, 3);
   });
 
   test('lerpVectors', () => {
     const alpha = 0.5;
     const threejsResult = v1t.clone().lerpVectors(v2t, v1t, alpha);
-    const threejsResultArray = [threejsResult.x, threejsResult.y].map(x => round(x, 4));
     const zk3dResult = v1z.clone().lerpVectors(v2z, v1z, ZK3D.Real64.from(alpha));
-    const zk3dResultArray = [zk3dResult.x.toNumber(), zk3dResult.y.toNumber()].map(x => round(x, 4));
-    expect(threejsResultArray).toEqual(zk3dResultArray);
+    expect(zk3dResult.x.toNumber()).toBeCloseTo(threejsResult.x, 4);
+    expect(zk3dResult.y.toNumber()).toBeCloseTo(threejsResult.y, 4);
   });
 
   test('negate', () => {
     const threejsResult = v1t.clone().negate();
-    const threejsResultArray = [threejsResult.x, threejsResult.y].map(x => round(x, 4));
     const zk3dResult = v1z.clone().negate();
-    const zk3dResultArray = [zk3dResult.x.toNumber(), zk3dResult.y.toNumber()].map(x => round(x, 4));
-    expect(threejsResultArray).toEqual(zk3dResultArray);
+    expect(zk3dResult.x.toNumber()).toBeCloseTo(threejsResult.x, 3);
+    expect(zk3dResult.y.toNumber()).toBeCloseTo(threejsResult.y, 3);
   });
+  
 
   test('divide', () => {
     const threejsResult = v1t.clone().divide(v2t);
-    const threejsResultArray = [threejsResult.x, threejsResult.y].map(x => round(x, 1));
     const zk3dResult = v1z.clone().divide(v2z);
-    const zk3dResultArray = [zk3dResult.x.toNumber(), zk3dResult.y.toNumber()].map(x => round(x, 1));
-    expect(threejsResultArray).toEqual(zk3dResultArray);
+    expect(zk3dResult.x.toNumber()).toBeCloseTo(threejsResult.x, 1);
+    expect(zk3dResult.y.toNumber()).toBeCloseTo(threejsResult.y, 1);
   });
 
   test('divideScalar', () => {
     const scalar = 3;
     const threejsResult = v1t.clone().divideScalar(scalar);
-    const threejsResultArray = [threejsResult.x, threejsResult.y].map(x => round(x, 3));
     const zk3dResult = v1z.clone().divideScalar(ZK3D.Real64.from(scalar));
-    const zk3dResultArray = [zk3dResult.x.toNumber(), zk3dResult.y.toNumber()].map(x => round(x, 3));
-    expect(threejsResultArray).toEqual(zk3dResultArray);
+    expect(zk3dResult.x.toNumber()).toBeCloseTo(threejsResult.x, 3);
+    expect(zk3dResult.y.toNumber()).toBeCloseTo(threejsResult.y, 3);
   });
 
 });
-
-
-
-
-
--- tests/vector3.test.ts
+++ tests/vector3.test.ts
@@ -1,8 +1,6 @@
 import * as ZK3D from '../src/index';
 import * as THREE from 'three';
 
-const round = (x: number, precision: number) => Math.round(x * Math.pow(10, precision)) / Math.pow(10, precision);
-
 describe('Vector3 operations', () => {
   const v1 = [0.001234, 2.345, 0.3456];
   const v2 = [4.567, 0.05678, 6.789];
@@ -14,104 +12,104 @@ describe('Vector3 operations', () => {
 
   test('vector addition', () => {
     const threejsResult = v1t.clone().add(v2t);
-    const threejsResultArray = [threejsResult.x, threejsResult.y, threejsResult.z].map(x => round(x, 4));
     const zk3dResult = v1z.clone().add(v2z);
-    const zk3dResultArray = [zk3dResult.x.toNumber(), zk3dResult.y.toNumber(), zk3dResult.z.toNumber()].map(x => round(x, 4));
-    expect(threejsResultArray).toEqual(zk3dResultArray);
+    expect(zk3dResult.x.toNumber()).toBeCloseTo(threejsResult.x, 4);
+    expect(zk3dResult.y.toNumber()).toBeCloseTo(threejsResult.y, 4);
+    expect(zk3dResult.z.toNumber()).toBeCloseTo(threejsResult.z, 4);
   });
 
   test('vector subtraction', () => {
     const threejsResult = v1t.clone().sub(v2t);
-    const threejsResultArray = [threejsResult.x, threejsResult.y, threejsResult.z].map(x => round(x, 4));
     const zk3dResult = v1z.clone().sub(v2z);
-    const zk3dResultArray = [zk3dResult.x.toNumber(), zk3dResult.y.toNumber(), zk3dResult.z.toNumber()].map(x => round(x, 4));
-    expect(threejsResultArray).toEqual(zk3dResultArray);
+    expect(zk3dResult.x.toNumber()).toBeCloseTo(threejsResult.x, 4);
+    expect(zk3dResult.y.toNumber()).toBeCloseTo(threejsResult.y, 4);
+    expect(zk3dResult.z.toNumber()).toBeCloseTo(threejsResult.z, 4);
   });
 
   test('vector multiplication', () => {
     const threejsResult = v1t.clone().multiply(v2t);
-    const threejsResultArray = [threejsResult.x, threejsResult.y, threejsResult.z].map(x => round(x, 2));
     const zk3dResult = v1z.clone().multiply(v2z);
-    const zk3dResultArray = [zk3dResult.x.toNumber(), zk3dResult.y.toNumber(), zk3dResult.z.toNumber()].map(x => round(x, 2));
-    expect(threejsResultArray).toEqual(zk3dResultArray);
+    expect(zk3dResult.x.toNumber()).toBeCloseTo(threejsResult.x, 2);
+    expect(zk3dResult.y.toNumber()).toBeCloseTo(threejsResult.y, 2);
+    expect(zk3dResult.z.toNumber()).toBeCloseTo(threejsResult.z, 2);
   });
 
   test('multiplication by scalar', () => {
     const scalar = 3;
     const threejsResult = v1t.clone().multiplyScalar(scalar);
-    const threejsResultArray = [threejsResult.x, threejsResult.y, threejsResult.z].map(x => round(x, 3));
     const zk3dResult = v1z.clone().multiplyScalar(ZK3D.Real64.from(scalar));
-    const zk3dResultArray = [zk3dResult.x.toNumber(), zk3dResult.y.toNumber(), zk3dResult.z.toNumber()].map(x => round(x, 3));
-    expect(threejsResultArray).toEqual(zk3dResultArray);
+    expect(zk3dResult.x.toNumber()).toBeCloseTo(threejsResult.x, 3);
+    expect(zk3dResult.y.toNumber()).toBeCloseTo(threejsResult.y, 3);
+    expect(zk3dResult.z.toNumber()).toBeCloseTo(threejsResult.z, 3);
   });
 
   test('dot product', () => {
-    const threejsResult = v1t.dot(v2t);
+    const threejsResult = v1t.clone().dot(v2t);
     const zk3dResult = v1z.clone().dot(v2z);
-    expect(round(threejsResult, 1)).toEqual(round(zk3dResult.toNumber(), 1));
+    expect(zk3dResult.toNumber()).toBeCloseTo(threejsResult, 1);
   });
 
   test('cross product', () => {
     const threejsResult = v1t.clone().cross(v2t);
-    const threejsResultArray = [threejsResult.x, threejsResult.y, threejsResult.z].map(x => round(x, 3));
     const zk3dResult = v1z.clone().cross(v2z);
-    const zk3dResultArray = [zk3dResult.x.toNumber(), zk3dResult.y.toNumber(), zk3dResult.z.toNumber()].map(x => round(x, 3));
-    expect(threejsResultArray).toEqual(zk3dResultArray);
+    expect(zk3dResult.x.toNumber()).toBeCloseTo(threejsResult.x, 3);
+    expect(zk3dResult.y.toNumber()).toBeCloseTo(threejsResult.y, 3);
+    expect(zk3dResult.z.toNumber()).toBeCloseTo(threejsResult.z, 3);
   });
 
   test('length squared', () => {
     const threejsResult = v1t.clone().lengthSq();
-    const zk3dResult = v1z.clone().lengthSq();
-    expect(round(threejsResult, 3)).toEqual(round(zk3dResult.toNumber(), 3));
+    const zk3dResult = v1z.clone().lengthSq().toNumber();
+    expect(zk3dResult).toBeCloseTo(threejsResult, 3);
   });
 
   test('distanceToSquared', () => {
     const threejsResult = v1t.clone().distanceToSquared(v2t);
-    const zk3dResult = v1z.clone().distanceToSquared(v2z);
-    expect(round(threejsResult, 3)).toEqual(round(zk3dResult.toNumber(), 3));
+    const zk3dResult = v1z.clone().distanceToSquared(v2z).toNumber();
+    expect(zk3dResult).toBeCloseTo(threejsResult, 3);
   });
 
   test('lerp', () => {
     const alpha = 0.5;
     const threejsResult = v1t.clone().lerp(v2t, alpha);
-    const threejsResultArray = [threejsResult.x, threejsResult.y, threejsResult.z].map(x => round(x, 3));
     const zk3dResult = v1z.clone().lerp(v2z, ZK3D.Real64.from(alpha));
-    const zk3dResultArray = [zk3dResult.x.toNumber(), zk3dResult.y.toNumber(), zk3dResult.z.toNumber()].map(x => round(x, 3));
-    expect(threejsResultArray).toEqual(zk3dResultArray);
+    expect(zk3dResult.x.toNumber()).toBeCloseTo(threejsResult.x, 3);
+    expect(zk3dResult.y.toNumber()).toBeCloseTo(threejsResult.y, 3);
+    expect(zk3dResult.z.toNumber()).toBeCloseTo(threejsResult.z, 3);
   });
 
   test('lerpVectors', () => {
     const alpha = 0.5;
     const threejsResult = v1t.clone().lerpVectors(v2t, v1t, alpha);
-    const threejsResultArray = [threejsResult.x, threejsResult.y, threejsResult.z].map(x => round(x, 3));
     const zk3dResult = v1z.clone().lerpVectors(v2z, v1z, ZK3D.Real64.from(alpha));
-    const zk3dResultArray = [zk3dResult.x.toNumber(), zk3dResult.y.toNumber(), zk3dResult.z.toNumber()].map(x => round(x, 3));
-    expect(threejsResultArray).toEqual(zk3dResultArray);
+    expect(zk3dResult.x.toNumber()).toBeCloseTo(threejsResult.x, 3);
+    expect(zk3dResult.y.toNumber()).toBeCloseTo(threejsResult.y, 3);
+    expect(zk3dResult.z.toNumber()).toBeCloseTo(threejsResult.z, 3);
   });
 
   test('negate', () => {
     const threejsResult = v1t.clone().negate();
-    const threejsResultArray = [threejsResult.x, threejsResult.y, threejsResult.z].map(x => round(x, 3));
     const zk3dResult = v1z.clone().negate();
-    const zk3dResultArray = [zk3dResult.x.toNumber(), zk3dResult.y.toNumber(), zk3dResult.z.toNumber()].map(x => round(x, 3));
-    expect(threejsResultArray).toEqual(zk3dResultArray);
+    expect(zk3dResult.x.toNumber()).toBeCloseTo(threejsResult.x, 3);
+    expect(zk3dResult.y.toNumber()).toBeCloseTo(threejsResult.y, 3);
+    expect(zk3dResult.z.toNumber()).toBeCloseTo(threejsResult.z, 3);
   });
 
   test('divide', () => {
     const threejsResult = v1t.clone().divide(v2t);
-    const threejsResultArray = [threejsResult.x, threejsResult.y, threejsResult.z].map(x => round(x, 1));
     const zk3dResult = v1z.clone().divide(v2z);
-    const zk3dResultArray = [zk3dResult.x.toNumber(), zk3dResult.y.toNumber(), zk3dResult.z.toNumber()].map(x => round(x, 1));
-    expect(threejsResultArray).toEqual(zk3dResultArray);
+    expect(zk3dResult.x.toNumber()).toBeCloseTo(threejsResult.x, 1);
+    expect(zk3dResult.y.toNumber()).toBeCloseTo(threejsResult.y, 1);
+    expect(zk3dResult.z.toNumber()).toBeCloseTo(threejsResult.z, 1);
   });
 
   test('divideScalar', () => {
     const scalar = 3;
     const threejsResult = v1t.clone().divideScalar(scalar);
-    const threejsResultArray = [threejsResult.x, threejsResult.y, threejsResult.z].map(x => round(x, 3));
     const zk3dResult = v1z.clone().divideScalar(ZK3D.Real64.from(scalar));
-    const zk3dResultArray = [zk3dResult.x.toNumber(), zk3dResult.y.toNumber(), zk3dResult.z.toNumber()].map(x => round(x, 3));
-    expect(threejsResultArray).toEqual(zk3dResultArray);
+    expect(zk3dResult.x.toNumber()).toBeCloseTo(threejsResult.x, 3);
+    expect(zk3dResult.y.toNumber()).toBeCloseTo(threejsResult.y, 3);
+    expect(zk3dResult.z.toNumber()).toBeCloseTo(threejsResult.z, 3);
   });
 
 });
